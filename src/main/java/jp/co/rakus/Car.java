c99f73d4cf0b5ea1a3dbaeb0d2428cf87d1af779
saitowataru <wataru.saito@rakus.co.jp>
2018-11-19 10:07:58 +0900

    新規カークラス追加

---
+++ src/main/java/jp/co/rakus/Car.java
@@ -0,0 +1,24 @@
+package jp.co.rakus;
+
+public class Car {
+
+	int speed;
+	String color;
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+}
